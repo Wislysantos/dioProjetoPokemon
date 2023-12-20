9e8d543f9580f96a67749226e2e078cf161ec7cb
Wislysantos <wislysantos@gmail.com>
2023-12-20 20:46:42 -0300

    transformando nossa lista de pokemon em uma lista de HTML

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,11 +3,39 @@ const offset = 0;
 const limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
+function convertToHtml(pokemon){
+    return`
+    <ol class="pokemons">
+    <li class="pokemon">
+        <span class="number">#001</span>
+        <span class="name">${pokemon.name}</span>
 
+        <div class="detail">
+            <ol class="types">
+                <li class="type">grass</li>
+                <li class="type">poison</li>
+            </ol>
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+            alt="${pokemon.name}">
+        </div>
+    </li>
+    </ol>
+    `
+}
+
+const pokemonList = document.getElementById('pokedex')
+//pokemonList.innerHTML += convertToHtml()
 
 fetch(url)
     .then((resp)=> resp.json())
-    .then((jsonBody)=> console.log(jsonBody))
+    .then((jsonBody)=> jsonBody.results)
+    .then((pokemonLi)=> {
+        for (let i = 0; i < pokemonLi.length; i++) {
+            const pokemon = pokemonLi[i];
+            pokemonList.innerHTML += convertToHtml(pokemon)
+            
+        }
+    })
     .catch((error)=> console.error(error))
     .finally(console.log('Sucesso!'))
 
