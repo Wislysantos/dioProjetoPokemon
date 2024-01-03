60eb4de0cf67dbae5f64cdcf70e57022071843f0
Wislysantos <wislysantos@gmail.com>
2024-01-03 19:36:05 -0300

    add paginação

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,42 @@
+const pokemonList = document.getElementById('pokedex')
+const loadPokemonButton = document.getElementById('LoadMore')
 
-function convertToHtml(pokemon){
-    return`
-    <ol class="pokemons">
-    <li class="pokemon ${pokemon.type}">
-        <span class="number">#${pokemon.num}</span>
-        <span class="name">${pokemon.name}</span>
+let offset = 0
+const  limit = 10
 
-        <div class="detail">
-            <ol class="types">
-                ${pokemon.types.map((type) =>`<li class="type ${type}">${type}</li>`).join('')}
+function loadPokemonItens(offset, limit){
+    pokeApi.convertToHTML(offset, limit).then((pokemons)=> {
+        const newHtml = pokemons.map((pokemon) => 
+            `
+            <ol class="pokemons">
+            <li class="pokemon ${pokemon.type}">
+                <span class="number">#${pokemon.num}</span>
+                <span class="name">${pokemon.name}</span>
+        
+                <div class="detail">
+                    <ol class="types">
+                        ${pokemon.types.map((type) =>`<li class="type ${type}">${type}</li>`).join('')}
+                    </ol>
+                    <img src=${pokemon.photo}
+                    alt="${pokemon.name}">
+                </div>
+            </li>
             </ol>
-            <img src=${pokemon.photo}
-            alt="${pokemon.name}">
-        </div>
-    </li>
-    </ol>
-    `
+            `
+        ).join('')
+        pokemonList.innerHTML += newHtml
+        })
+        .catch((error)=> console.error(error))
+        .finally(console.log('Sucesso!'))
+    
 }
 
-const pokemonList = document.getElementById('pokedex')
+loadPokemonItens(offset, limit)
 
-pokeApi.convertToHTML().then((pokemons)=> {
-    pokemonList.innerHTML += (pokemons.map(convertToHtml)).join('')
-    })
-    .catch((error)=> console.error(error))
-    .finally(console.log('Sucesso!'))
+loadPokemonButton.addEventListener('click', ()=>{
+    offset += limit
+    loadPokemonItens(offset, limit)
+})
 
 
 //console.log('Sucesso!')
