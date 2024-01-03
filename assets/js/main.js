6a069df3d59df34d7956d71e9c53d278f4bcd3f2
Wislysantos <wislysantos@gmail.com>
2024-01-03 15:54:55 -0300

    manipulação de requisições

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,15 +7,15 @@ function convertToHtml(pokemon){
     return`
     <ol class="pokemons">
     <li class="pokemon">
-        <span class="number">#001</span>
+        <span class="number">#${pokemon.order}</span>
         <span class="name">${pokemon.name}</span>
 
         <div class="detail">
             <ol class="types">
-                <li class="type">grass</li>
+                <li class="type">  sa</li>
                 <li class="type">poison</li>
             </ol>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+            <img src=${pokemon.sprites.other.dream_world.front_default}
             alt="${pokemon.name}">
         </div>
     </li>
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,10 +1,17 @@
 const pokeApi = {}
 
+pokeApi.getPokemonDetali = (pokemon) => {
+    return fetch(pokemon.url).then(resp => resp.json())
+}
+
 pokeApi.convertToHTML = (offset = 0, limit = 10) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
     return fetch(url)
     .then((resp) => resp.json())
     .then((jsonBody) => jsonBody.results)
+    .then((pokemons)=> pokemons.map(pokeApi.getPokemonDetali))
+    .then((detaliRequests) => Promise.all(detaliRequests))
+    .then((pokemonDetali) => pokemonDetali)
     .catch((error) => console.error(error))
 }
